97306094ea486e320942c29f4204e0a095809d29
Giovillando <121701082+Giovillando@users.noreply.github.com>
2024-05-26 17:53:20 +0700

    make test pass

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -26,7 +26,7 @@ function LoginPage() {
           Lets
           <strong>Play MLBB</strong>
           <br />
-          Having Fun and Discussion about MLBB in Here.
+          Having Fun and Discussion in Here All.
         </h2>
         <LoginInput login={onLogin} />
         <p>
