34a212b899e20b4032235aa2defc97fa84a2c4b3
Giovillando <121701082+Giovillando@users.noreply.github.com>
2024-05-27 01:42:42 +0700

    make test pass

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -4,7 +4,11 @@ import { useState } from "react";
 function useInput(defaultValue = "") {
   const [value, setValue] = useState(defaultValue);
 
-  return [value, setValue];
+  function handleValueChange({ target }) {
+    setValue(target.value);
+  }
+
+  return [value, handleValueChange, setValue];
 }
 
 export default useInput;
